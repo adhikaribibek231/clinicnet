13f1181edd8b09fdbb7e0976dfae37b9ef0af2fb
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-04-18 12:08:07 +0545

    appointment model created

--- clinic/models.py
+++ clinic/models.py
@@ -22,3 +22,22 @@ class Doctor(models.Model):
 
     def __str__(self):
         return f"Dr. {self.first_name} {self.last_name} - {self.specialty}"
+    
+# class Staff(models.Model):
+#     first_name = models.CharField(max_length=50)
+#     last_name = models.CharField(max_length=50)
+#     role = models.CharField(max_length=50, choices=[('admin', 'Admin'), ('receptionist', 'Receptionist')])
+#     mobile_number = models.CharField(max_length=15, unique=True)
+#     email = models.EmailField(unique=True)
+
+#     def __str__(self):
+#             return f"{self.first_name} {self.last_name} - {self.role.capitalize()}"
+        
+class Appointment(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    appointment_date = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=[('scheduled', 'Scheduled'), ('completed', 'Completed'), ('canceled', 'Canceled')])
+
+    def __str__(self):
+        return f"Appointment: {self.patient} with {self.doctor} on {self.appointment_date}"
